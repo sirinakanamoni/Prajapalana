fbe0cdf41983ff3d7945303a77011a555ab8006f
sirinakanamoni <166201658+sirinakanamoni@users.noreply.github.com>
2024-09-29 17:22:45 +0530

    Add files via upload

---
+++ src/app/Models/Ppaform.ts
@@ -0,0 +1,11 @@
+export class Ppaform{
+    public applicationNumber!:number;
+    public applicantName:string="";
+    public gender:string="";
+    public age!:number;
+    public mobileNumber!:number;
+    public dob!:Date;
+    public aadharNumber!:number;
+    public rationCardNumber:string="";
+    public catedory:string="";
+}
